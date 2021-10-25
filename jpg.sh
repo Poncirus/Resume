0f029336bdbe1c23e9ae748477a8ad9445e1ef1f
Liao Hanwen <login@liaohanwen.com>
2021-10-25 22:45:04 +0800

    Complete Yuanfudao

--- jpg.sh
+++ jpg.sh
@@ -1,3 +1,7 @@
 rm resume-cn.jpg
-pdftoppm -jpeg -r 300 resume-cn.pdf resume-cn
+pdftoppm -jpeg -r 300 -hide-annotations resume-cn.pdf resume-cn
 mv resume-cn-1.jpg resume-cn.jpg
+
+rm resume.jpg
+pdftoppm -jpeg -r 300 -hide-annotations resume.pdf resume
+mv resume-1.jpg resume.jpg
